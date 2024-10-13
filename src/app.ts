33210b9f26c5659a81ffbbda75e3bb0c5e850be8
Lucas Vieira Urquiza <urquizaxd@gmail.com>
2024-10-13 19:28:37 -0300

    feat: Resolve CORS problem

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import lightRoutes from './routes/lightRoutes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
